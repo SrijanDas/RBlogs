51cdeed031fb87e7c2aebfbe8b8846e2e10a1cd3
srijan1805 <srijan@dezyit.com>
2024-12-23 22:51:44 +0530

    Refractor: user readable error messages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -31,7 +31,9 @@ const FormSchema = z.object({
     email: z.string().email({
         message: "Invalid email address.",
     }),
-    password: z.string(),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long.",
+    }),
 });
 
 function LoginPage() {
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -26,12 +26,23 @@ import { toast } from "sonner";
 import { z } from "zod";
 
 const FormSchema = z.object({
-    name: z.string().min(3).max(50),
+    name: z
+        .string()
+        .min(3, {
+            message: "Name must be at least 3 characters long.",
+        })
+        .max(50, {
+            message: "Name must be at most 50 characters long.",
+        }),
     email: z.string().email({
         message: "Invalid email address.",
     }),
-    password: z.string(),
-    confirmPassword: z.string(),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long.",
+    }),
+    confirmPassword: z.string().min(6, {
+        message: "Password must be at least 6 characters long.",
+    }),
 });
 
 function RegisterPage() {
